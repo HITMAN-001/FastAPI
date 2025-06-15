11d2e3a976fcd525f34152c3a3df556013479326
Rohan Shirude <rohan92.pune@gmail.com>
2025-06-15 16:16:21 +0530

    Third Commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from models import Item, User
 
 # In-memory storage
 items: List[Item] = []
-users: List[User] = [], User
+users: List[User] = []
 
 # Create a FastAPI instance
 app = FastAPI()
@@ -48,4 +48,4 @@ if __name__ == "__main__":
     import uvicorn
     import os
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
